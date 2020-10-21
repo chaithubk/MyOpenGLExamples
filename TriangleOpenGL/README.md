3b94dad2558a56b6da8e0eabe05b14187c2adab0
Chaithanya, Krishna <chaithubkts@gmail.com>
2020-10-21 15:35:00 +0530

    Setup the minimal working example program. This example is from the 'Introduction to Modern OpenGL' training.

--- TriangleOpenGL/README.md
+++
@@ -1,4 +0,0 @@
-# Original source
-https://gist.github.com/f57764cab5033a688cb2b6996a98e4b3.git
-
-Made changes to project include & linker dependencies as per the latest OpenGL libraries. 
--- TriangleOpenGL/main.cpp
+++ TriangleOpenGL/main.cpp
@@ -1,190 +1,137 @@
-// CPP program to render a triangle using Shaders
-#include <GL/glew.h>
-#include <glut.h>
-#include <GL/gl.h>
-#include <GL/glu.h>
-//#include <GL/glext.h>
+//STANDARD
+#include<iostream>
+#include<sstream>
+#include<stdlib.h>
+#include<time.h>
 
-#include <iostream>
-#include <string>
+//GLEW
+#define GLEW_STATIC
+#include<GL\glew.h>
+
+//GLFW
+#include<GLFW\glfw3.h>
+
+//OPENGL MATH GLM
+#include <glm/vec3.hpp> // glm::vec3
+#include <glm/vec4.hpp> // glm::vec4
+#include <glm/mat4x4.hpp> // glm::mat4
+#include <glm/gtc/matrix_transform.hpp> // glm::translate, glm::rotate, glm::scale, glm::perspective
+#include <glm/gtc/type_ptr.hpp>
 
 using namespace std;
 
-string vertexShader = R"(
-    #version 430
-    in vec3 pos;
-    void main()
-    {
-        gl_Position = vec4(pos, 1);
-    }
-)";
+//GLOBALS
+const char* APP_TITLE = "Traingle OpenGL";
+const int gWindowWidth = 1920;
+const int gWindowHeight = 1080;
+GLFWwindow* gWindow = NULL;
 
-string fragmentShader = R"(
-    #version 430
-    void main() 
-    {
-        gl_FragColor = vec4(1, 1, 1, 1); // white color
-    }
-)";
+void glfw_onKey(GLFWwindow* window, int key, int scancode, int action, int mode);
+void showFPS(GLFWwindow* window);
+bool initOpenGL();
 
-// Compile and create shader object and returns its id
-GLuint compileShaders(string shader, GLenum type)
+int main()
 {
+	if (!initOpenGL())
+	{
+		cerr << "GLFW initialization failed" << endl;
+		return -1;
+	}
 
-    const char* shaderCode = shader.c_str();
-    GLuint shaderId = glCreateShader(type);
+	// Main loop
+	while (!glfwWindowShouldClose(gWindow))
+	{
+		showFPS(gWindow);
 
-    if (shaderId == 0)
-    { // Error: Cannot create shader object
-        cout << "Error creating shaders!";
-        return 0;
-    }
+		glfwPollEvents();
 
-    // Attach source code to this object
-    glShaderSource(shaderId, 1, &shaderCode, NULL);
-    glCompileShader(shaderId); // compile the shader object
+		glClear(GL_COLOR_BUFFER_BIT);
 
-    GLint compileStatus;
+		glfwSwapBuffers(gWindow);
+	}
 
-    // check for compilation status
-    glGetShaderiv(shaderId, GL_COMPILE_STATUS, &compileStatus);
+	//TERMINATE GLFW
+	glfwTerminate();
 
-    if (!compileStatus)
-    { // If compilation was not successfull
-        int length;
-        glGetShaderiv(shaderId, GL_INFO_LOG_LENGTH, &length);
-        char* cMessage = new char[length];
-
-        // Get additional information
-        glGetShaderInfoLog(shaderId, length, &length, cMessage);
-        cout << "Cannot Compile Shader: " << cMessage;
-        delete[] cMessage;
-        glDeleteShader(shaderId);
-        return 0;
-    }
-
-    return shaderId;
+	return 0;
 }
 
-// Creates a program containing vertex and fragment shader
-// links it and returns its ID
-GLuint linkProgram(GLuint vertexShaderId, GLuint fragmentShaderId)
+//FUNCTIONS
+void glfw_onKey(GLFWwindow* window, int key, int scancode, int action, int mode)
 {
-    GLuint programId = glCreateProgram(); // crate a program
-
-    if (programId == 0)
-    {
-        cout << "Error Creating Shader Program";
-        return 0;
-    }
-
-    // Attach both the shaders to it
-    glAttachShader(programId, vertexShaderId);
-    glAttachShader(programId, fragmentShaderId);
-
-    // Create executable of this program
-    glLinkProgram(programId);
-
-    GLint linkStatus;
-
-    // Get the link status for this program
-    glGetProgramiv(programId, GL_LINK_STATUS, &linkStatus);
-
-    if (!linkStatus)
-    { // If the linking failed
-        cout << "Error Linking program";
-        glDetachShader(programId, vertexShaderId);
-        glDetachShader(programId, fragmentShaderId);
-        glDeleteProgram(programId);
-
-        return 0;
-    }
-
-    return programId;
+	if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+	{
+		glfwSetWindowShouldClose(window, GL_TRUE);
+	}
 }
 
-// Load data in VBO (Vertex Buffer Object) and return the vbo's id
-GLuint loadDataInBuffers()
+void showFPS(GLFWwindow* window)
 {
-    GLfloat vertices[] = {// triangle vertex coordinates
-                          -0.5, -0.5, 0,
-                          0.5, -0.5, 0,
-                          0, 0.5, 0 };
+	static double previousSeconds = 0.0;
+	static int frameCount = 0;
+	double elpasedSeconds;
+	double currentSeconds = glfwGetTime(); //
 
-    GLuint vboId;
+	elpasedSeconds = currentSeconds - previousSeconds;
 
-    // allocate buffer sapce and pass data to it
-    glGenBuffers(1, &vboId);
-    glBindBuffer(GL_ARRAY_BUFFER, vboId);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+	// limit text update 4 times per second 
+	if (elpasedSeconds > 0.25)
+	{
+		previousSeconds = currentSeconds;
+		double fps = (double)frameCount / elpasedSeconds;
+		double msPerFrame = 1000.0 / fps;
 
-    // unbind the active buffer
-    glBindBuffer(GL_ARRAY_BUFFER, 0);
+		std::ostringstream outs;
+		outs.precision(3);
+		outs << std::fixed
+			<< APP_TITLE << "   "
+			<< "FPS: " << fps << "   "
+			<< "Frame Time: " << msPerFrame << "(ms)";
 
-    return vboId;
+		glfwSetWindowTitle(window, outs.str().c_str());
+
+		frameCount = 0;
+	}
+
+	frameCount++;
 }
 
-// Initialize and put everything together
-void init()
+bool initOpenGL()
 {
-    // clear the framebuffer each frame with black color
-    glClearColor(0, 0, 0, 0);
+	if (!glfwInit())
+	{
+		cerr << "GLFW Initialization failed" << endl;
+		return false;
+	}
 
-    GLuint vboId = loadDataInBuffers();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+	glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
 
-    GLuint vShaderId = compileShaders(vertexShader, GL_VERTEX_SHADER);
-    GLuint fShaderId = compileShaders(fragmentShader, GL_FRAGMENT_SHADER);
+	gWindow = glfwCreateWindow(gWindowWidth, gWindowHeight, APP_TITLE, nullptr, nullptr);
 
-    GLuint programId = linkProgram(vShaderId, fShaderId);
+	if (gWindow == nullptr)
+	{
+		glfwTerminate();
+		throw("Error in creating window!");
+		return false;
+	}
 
-    // Get the 'pos' variable location inside this program
-    GLuint posAttributePosition = glGetAttribLocation(programId, "pos");
+	glfwMakeContextCurrent(gWindow);
 
-    GLuint vaoId;
-    glGenVertexArrays(1, &vaoId); // Generate VAO  (Vertex Array Object)
+	glfwSetKeyCallback(gWindow, glfw_onKey);
 
-    // Bind it so that rest of vao operations affect this vao
-    glBindVertexArray(vaoId);
+	glewExperimental = GL_TRUE;
+	if (glewInit() != GLEW_OK)
+	{
+		glfwTerminate();
+		throw "Error in GLEW INIT!";
+		return false;
+	}
 
-    // buffer from which 'pos' will receive its data and the format of that data
-    glBindBuffer(GL_ARRAY_BUFFER, vboId);
-    glVertexAttribPointer(posAttributePosition, 3, GL_FLOAT, false, 0, 0);
+	glClearColor(0.2f, 0.3f, 0.2f, 1.0f);
 
-    // Enable this attribute array linked to 'pos'
-    glEnableVertexAttribArray(posAttributePosition);
-
-    // Use this program for rendering.
-    glUseProgram(programId);
-}
-
-// Function that does the drawing
-// glut calls this function whenever it needs to redraw
-void display()
-{
-    // clear the color buffer before each drawing
-    glClear(GL_COLOR_BUFFER_BIT);
-
-    // draw triangles starting from index 0 and
-    // using 3 indices
-    glDrawArrays(GL_TRIANGLES, 0, 3);
-
-    // swap the buffers and hence show the buffers
-    // content to the screen
-    glutSwapBuffers();
-}
-
-// main function
-// sets up window to which we'll draw
-int main(int argc, char** argv)
-{
-    glutInit(&argc, argv);
-    glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE);
-    glutInitWindowSize(500, 500);
-    glutInitWindowPosition(100, 100);
-    glutCreateWindow("Triangle Using OpenGL");
-    glewInit();
-    init();
-    glutDisplayFunc(display);
-    glutMainLoop();
-    return 0;
+	return true;
 }
