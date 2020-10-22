d8972d82ac1a19983c17e5d00fed0a28d6f51b90
Chaithanya, Krishna <chaithubkts@gmail.com>
2020-10-23 00:19:31 +0530

    Set position uniform variable
    To oscillate the cube in clock wise direction, a kind of little animation.

--- HelloShader/src/main.cpp
+++ HelloShader/src/main.cpp
@@ -92,10 +92,14 @@ int main()
 
 		shaderProgram.use();
 
+		// NOTE: Set the uniform after the .use()
 		GLfloat time = glfwGetTime();
 		GLfloat blueColor = (sin(time) / 2) + 0.5f;
+		glm::vec2 pos;
+		pos.x = sin(time) / 2;
+		pos.y = cos(time) / 2;
 
-		// NOTE: Set the uniform after the .use()
+		shaderProgram.setUniform("posOffset", pos);
 		shaderProgram.setUniform("vertColor", glm::vec4(0.0f, 0.0f, blueColor, 1.0f));
 
 		glBindVertexArray(vao);
